82636804f412a889973066c40d8db2e15f1aea39
franPdev <francoarielpaez@gmail.com>
2025-06-28 20:35:11 -0300

    Elimina archivos CSS, JS e imágenes no utilizados para limpiar el proyecto

--- js/utils.js
+++
@@ -1,9 +0,0 @@
-function ordenarCursosPorTitulo(lista, orden = "asc") {
-  return [...lista].sort((a, b) =>
-    orden === "asc"
-      ? a.titulo.localeCompare(b.titulo)
-      : b.titulo.localeCompare(a.titulo)
-  );
-}
-
-module.exports = { ordenarCursosPorTitulo };
